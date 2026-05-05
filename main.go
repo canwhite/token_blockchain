4260e4a46dabac9b2e09779db0cc558c71907270
qiaochao <752716886@qq.com>
2026-05-05 15:49:54 +0800

    feat: info about gin

--- main.go
+++ main.go
@@ -16,18 +16,25 @@ type Message struct {
 	BPM int
 }
 
+//get block chain
 func handleGetBlockchain(c *gin.Context) {
+	//marshal转json
 	bytes, err := json.MarshalIndent(blockchain.Blockchain, "", "  ")
 	if err != nil {
+		//都是以Status开头，返回的json数据可以直接用gin.H
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
+	//PS：C.Data返回需要手动设置ContentType
 	c.Data(http.StatusOK, "application/json", bytes)
 }
 
+//set block chain
 func handleWriteBlock(c *gin.Context) {
 	var m Message
+	//将请求体解析并自动绑定到结构体上
 	if err := c.ShouldBindJSON(&m); err != nil {
+		//常用的也就这三个，OK、BadRequest、InternalServerError
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
